1240770c53b22c6f3432de3a4c05ac2e0234724b
Hafoza <163436847+real-Hafez@users.noreply.github.com>
2024-07-26 23:47:41 +0300

    Add anim To trending peoble

--- lib/tending_peoble_movies_series/widgets/Popular_Actors.dart
+++ lib/tending_peoble_movies_series/widgets/Popular_Actors.dart
@@ -1,3 +1,5 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cinema_x/ApiConfig.dart';
 import 'package:cinema_x/tending_peoble_movies_series/models/Person_Model.dart';
@@ -12,6 +14,7 @@ class PopularActors extends StatelessWidget {
     return SizedBox(
       height: MediaQuery.of(context).size.height * .3,
       child: ListView.builder(
+        physics: const BouncingScrollPhysics(),
         scrollDirection: Axis.horizontal,
         itemCount: personModel.length,
         itemBuilder: (context, index) {
@@ -30,19 +33,46 @@ class PopularActors extends StatelessWidget {
                       child: CachedNetworkImage(
                         imageUrl:
                             'https://image.tmdb.org/t/p/original${personModel[index].profilePath}',
-                        fit: BoxFit.fill,
+                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
                 ),
                 SizedBox(
-                  height: MediaQuery.of(context).size.height * .03,
+                  height: MediaQuery.of(context).size.height * .02,
                 ),
-                Text(
-                  personModel[index].name,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: MediaQuery.of(context).size.height * .02,
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: MediaQuery.of(context).size.width * .04,
+                  ),
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      WavyAnimatedText(
+                        personModel[index].name,
+                        textStyle: TextStyle(
+                          color: Colors.white,
+                          fontSize: MediaQuery.of(context).size.height * .026,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        speed: const Duration(milliseconds: 309),
+                      ),
+                      ColorizeAnimatedText(
+                        personModel[index].name,
+                        textStyle: TextStyle(
+                          fontSize: MediaQuery.of(context).size.height * .026,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        colors: [
+                          Colors.purple,
+                          Colors.blue,
+                          Colors.yellow,
+                          Colors.red,
+                        ],
+                        speed: const Duration(milliseconds: 200),
+                      ),
+                    ],
+                    isRepeatingAnimation: false,
+                    repeatForever: false,
                   ),
                 ),
               ],
